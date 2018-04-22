908642aee18ef3ba03c2569696e3475f8cfa7c37
sapirsarig <sapirsarig@gmail.com>
2018-04-23 00:48:01 +0300

    update guset screen

--- FinalProject - Client/final_project_client/src/GuestScreen/GuestScreen.js
+++ FinalProject - Client/final_project_client/src/GuestScreen/GuestScreen.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import Login from './Login/Login.js';
 import './GuestScreen.css';
-import {Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import App from '../App';
 
 
 
 class GuestScreen extends Component {
 
-    render()
-    {
-        return(
+    render() {
+        return (
 
-            <div className = "guestScreenWrapper">
-                <Login />
-                <div className="explanationWrapper">
-                    <h1>
-                        <div className = "siteHeader"> It's A Deal !! </div>
-                    </h1>
-                    <div className="explanation"> 
+            <div className="guestScreenWrapper">
+                    <div className="siteHeader"><h1> It's A Deal !! </h1></div>
+
+                    <div>
                         Are you a star on social networks like Facebook or Instagram?<br></br>
-                        Do you own a business and looking for an opportunity to sell your products?<br></br>                   
+                        Do you own a business and looking for an opportunity to sell your products?<br></br>
                         Use our website to connect with each other!
+                             </div>
+                    <div className="explanation">
+                        <Login />
+                        <div>
+                            <iframe width="420" height="345" src="https://www.youtube.com/embed/FlsCjmMhFmw" />
+                        </div>
                     </div>
-                    <iframe width="420" height="345" src="https://www.youtube.com/embed/FlsCjmMhFmw">
-                    </iframe>
-                    
-                </div>
+
                 {/* <Route exact path="/" component={App} /> */}
             </div>
 
