fe2e5e2c264629025c5ab01b9d8300f380a0391d
Meitaloly <37400849+Meitaloly@users.noreply.github.com>
2018-09-11 22:23:34 +0300

    tg

---
+++ FinalProject - Client/final_project_client/src/components/negotiation/NewMessageArea.js
@@ -0,0 +1,43 @@
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Negotiation.css';
+
+class NewMessageArea extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        //const { ChatId, User } = this.props;
+        ChatId = 1;
+        this.props.addNewMessageToList(this.state.message);
+        this.setState({ message: '' });
+    }
+
+    handleChange(event) {
+        this.setState({ message: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="newMessageArea" onSubmit={this.handleSubmit}>
+                <div className="newMessageLabel">
+                    Write message:
+                </div>
+                <div>
+                    <textarea className="messageTextArea" value={this.state.message} onChange={this.handleChange} />
+                </div>
+                <button className="sendBtn" onClick={this.handleSubmit} disabled={this.state.message.length === 0}> Send </button>
+            </div>
+        );
+    }
+}
+
+export default NewMessageArea;
